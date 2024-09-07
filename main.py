7f966421ab4e6b9d5aa60addaf7cc8b8a1982e36
Gurveen <gurveenkaursahni@gmail.com>
2024-09-06 21:28:12 -0400

    mask

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ while True:
     # HSV is prefered over RGB for color detection tasks because it separates color information (hue) from intensity (value)
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+    # Returns a mask/binary mask ( a binary image that defines which pixels in the given image should be considered (white or 1) and which should be ignored (black or 0).)
+    mask = cv2.inRange(hsv, )
+    
     cv2.imshow('Frame', frame)
 
     if (cv2.waitKey(1) & 0xFF) == ord('q'):
